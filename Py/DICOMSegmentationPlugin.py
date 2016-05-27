42da8391f4adbe133ab408ceab0b5bda2b601482
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-05-27 17:00:02 -0400

    STYLE: Explicitly import "vtkTeem" to use "vtkImageLabelCombine"

--- Py/DICOMSegmentationPlugin.py
+++ Py/DICOMSegmentationPlugin.py
@@ -1,6 +1,11 @@
 import os
 import string
-import vtk, qt, ctk, slicer
+import vtk
+import qt
+import ctk
+import slicer
+import vtkTeem
+
 from DICOMLib import DICOMPlugin
 from DICOMLib import DICOMLoadable
 
@@ -281,7 +286,7 @@ class DICOMSegmentationPluginClass(DICOMPlugin):
     if labelNode:
       volumeLogic = slicer.modules.volumes.logic()
       mergeNode = volumeLogic.CloneVolume(labelNode, seriesName + "-label")
-      combiner = slicer.vtkImageLabelCombine()
+      combiner = vtkTeem.vtkImageLabelCombine()
       for segmentNode in segmentNodes:
         combiner.SetInputConnection(0, mergeNode.GetImageDataConnection() )
         combiner.SetInputConnection(1, segmentNode.GetImageDataConnection() )
