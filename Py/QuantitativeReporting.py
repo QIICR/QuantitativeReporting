b0576c8035f58ea0ce8d09fbe4d068b9136e03d4
Christian <christianherz1985@googlemail.com>
2016-12-19 14:42:13 -0500

    STYLE: renamed from areTerminologyInformationValid to isTerminologyInformationValid

--- Py/QuantitativeReporting.py
+++ Py/QuantitativeReporting.py
@@ -961,17 +961,17 @@ class CustomSegmentStatisticsLogic(SegmentStatisticsLogic):
     # Both Category and Type are required, so return if not available
     # TODO: consider populating to "Tissue" if not available?
     categoryObject = terminologyEntry.GetCategoryObject()
-    if categoryObject is None or not self.areTerminologyInformationValid(categoryObject):
+    if categoryObject is None or not self.isTerminologyInformationValid(categoryObject):
       return {}
     segmentData["SegmentedPropertyCategoryCodeSequence"] = self.getJSONFromVtkSlicerTerminology(categoryObject)
 
     typeObject = terminologyEntry.GetTypeObject()
-    if typeObject is None or not self.areTerminologyInformationValid(typeObject):
+    if typeObject is None or not self.isTerminologyInformationValid(typeObject):
       return {}
     segmentData["SegmentedPropertyTypeCodeSequence"] = self.getJSONFromVtkSlicerTerminology(typeObject)
 
     modifierObject = terminologyEntry.GetTypeModifierObject()
-    if modifierObject is not None and self.areTerminologyInformationValid(modifierObject):
+    if modifierObject is not None and self.isTerminologyInformationValid(modifierObject):
       segmentData["SegmentedPropertyTypeModifierCodeSequence"] = self.getJSONFromVtkSlicerTerminology(modifierObject)
 
     return segmentData
@@ -981,16 +981,16 @@ class CustomSegmentStatisticsLogic(SegmentStatisticsLogic):
     segmentData = dict()
 
     regionObject = terminologyEntry.GetAnatomicRegionObject()
-    if regionObject is None or not self.areTerminologyInformationValid(regionObject):
+    if regionObject is None or not self.isTerminologyInformationValid(regionObject):
       return {}
     segmentData["AnatomicRegionSequence"] = self.getJSONFromVtkSlicerTerminology(regionObject)
 
     regionModifierObject = terminologyEntry.GetAnatomicRegionModifierObject()
-    if regionModifierObject is not None and self.areTerminologyInformationValid(regionModifierObject):
+    if regionModifierObject is not None and self.isTerminologyInformationValid(regionModifierObject):
       segmentData["AnatomicRegionModifierSequence"] = self.getJSONFromVtkSlicerTerminology(regionModifierObject)
     return segmentData
 
-  def areTerminologyInformationValid(self, termTypeObject):
+  def isTerminologyInformationValid(self, termTypeObject):
     return all(t is not None for t in [termTypeObject.GetCodeValue(), termTypeObject.GetCodingScheme(), termTypeObject.GetCodeMeaning()])
 
   def getJSONFromVtkSlicerTerminology(self, termTypeObject):
