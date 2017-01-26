d4dca5d32957c17aec717f4862bbdd7ad40009cf
Christian Herz <christianherz1985@gmail.com>
2017-01-26 15:33:39 +0000

    Updates LICENSE.md
    Auto commit by GitBook Editor

--- Py/startSlicelet.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-SLICER="/Applications/Slicer.app/Contents/MacOS/Slicer"
-
-$SLICER --python-code "from QuantitativeReporting import QuantitativeReportingSlicelet; slicelet=QuantitativeReportingSlicelet();" --no-splash --no-main-window
