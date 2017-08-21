e73db5b53ce52b7a32a67ae780ecf082a1fdb5c7
Christian Herz <christianherz1985@googlemail.com>
2017-08-21 10:39:53 -0400

    ENH: adapt to code changes of SegmentStatistics integration into Slicer

--- QuantitativeReporting.py
+++ QuantitativeReporting.py
@@ -23,8 +23,6 @@ from SlicerDevelopmentToolboxUtils.buttons import CrosshairButton
 
 from SegmentEditor import SegmentEditorWidget
 from SegmentStatistics import SegmentStatisticsLogic, SegmentStatisticsParameterEditorDialog
-from SegmentStatisticsCalculators import ScalarVolumeSegmentStatisticsCalculator
-from SegmentStatisticsCalculators import SegmentStatisticsCalculatorBase
 
 from DICOMLib.DICOMWidgets import DICOMDetailsWidget
 
@@ -879,7 +877,11 @@ class QuantitativeReportingSegmentEditorLogic(ScriptedLoadableModuleLogic):
 
 class CustomSegmentStatisticsLogic(SegmentStatisticsLogic):
 
-  registeredCalculators = [ScalarVolumeSegmentStatisticsCalculator]
+  @staticmethod
+  def getDICOMTriplet(codeValue, codingSchemeDesignator, codeMeaning):
+    return {'CodeValue':codeValue,
+            'CodingSchemeDesignator': codingSchemeDesignator,
+            'CodeMeaning': codeMeaning}
 
   @property
   def statistics(self):
@@ -959,16 +961,16 @@ class CustomSegmentStatisticsLogic(SegmentStatisticsLogic):
     categoryObject = terminologyEntry.GetCategoryObject()
     if categoryObject is None or not self.isTerminologyInformationValid(categoryObject):
       return {}
-    segmentData["SegmentedPropertyCategoryCodeSequence"] = self.getJSONFromVtkSlicerTerminology(categoryObject)
+    segmentData["SegmentedPropertyCategoryCodeSequence"] = self.getJSONFromVTKSlicerTerminology(categoryObject)
 
     typeObject = terminologyEntry.GetTypeObject()
     if typeObject is None or not self.isTerminologyInformationValid(typeObject):
       return {}
-    segmentData["SegmentedPropertyTypeCodeSequence"] = self.getJSONFromVtkSlicerTerminology(typeObject)
+    segmentData["SegmentedPropertyTypeCodeSequence"] = self.getJSONFromVTKSlicerTerminology(typeObject)
 
     modifierObject = terminologyEntry.GetTypeModifierObject()
     if modifierObject is not None and self.isTerminologyInformationValid(modifierObject):
-      segmentData["SegmentedPropertyTypeModifierCodeSequence"] = self.getJSONFromVtkSlicerTerminology(modifierObject)
+      segmentData["SegmentedPropertyTypeModifierCodeSequence"] = self.getJSONFromVTKSlicerTerminology(modifierObject)
 
     return segmentData
 
@@ -979,21 +981,20 @@ class CustomSegmentStatisticsLogic(SegmentStatisticsLogic):
     regionObject = terminologyEntry.GetAnatomicRegionObject()
     if regionObject is None or not self.isTerminologyInformationValid(regionObject):
       return {}
-    segmentData["AnatomicRegionSequence"] = self.getJSONFromVtkSlicerTerminology(regionObject)
+    segmentData["AnatomicRegionSequence"] = self.getJSONFromVTKSlicerTerminology(regionObject)
 
     regionModifierObject = terminologyEntry.GetAnatomicRegionModifierObject()
     if regionModifierObject is not None and self.isTerminologyInformationValid(regionModifierObject):
-      segmentData["AnatomicRegionModifierSequence"] = self.getJSONFromVtkSlicerTerminology(regionModifierObject)
+      segmentData["AnatomicRegionModifierSequence"] = self.getJSONFromVTKSlicerTerminology(regionModifierObject)
     return segmentData
 
   def isTerminologyInformationValid(self, termTypeObject):
     return all(t is not None for t in [termTypeObject.GetCodeValue(), termTypeObject.GetCodingSchemeDesignator(),
                                        termTypeObject.GetCodeMeaning()])
 
-  def getJSONFromVtkSlicerTerminology(self, termTypeObject):
-    return SegmentStatisticsCalculatorBase.getDICOMTriplet(termTypeObject.GetCodeValue(),
-                                                           termTypeObject.GetCodingSchemeDesignator(),
-                                                           termTypeObject.GetCodeMeaning())
+  def getJSONFromVTKSlicerTerminology(self, termTypeObject):
+    return self.getDICOMTriplet(termTypeObject.GetCodeValue(), termTypeObject.GetCodingSchemeDesignator(),
+                                termTypeObject.GetCodeMeaning())
 
   def generateJSON4DcmSR(self, dcmSegmentationFile, sourceVolumeNode):
     measurements = []
